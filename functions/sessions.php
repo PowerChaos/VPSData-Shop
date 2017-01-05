061a6199d2fee4d4074d557874136c1327d941ec
PowerChaos <PowerChaos@users.noreply.github.com>
2017-01-05 13:47:26 +0100

    Fixed Session Errors and locations

--- functions/sessions.php
+++ functions/sessions.php
@@ -1,10 +1,14 @@
 <?php
 function sesions()
 {
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}	
 if ($_GET['action'] == "logout")
 {
     $_SESSION = array();
     session_destroy();
+	session_start();
 	session_regenerate_id(true);
     $_SESSION['ERROR'] = "Succesvol Uitgelogd";
 }
@@ -28,6 +32,11 @@ $_SESSION[ERROR] =  "Uw wachtwoord is gewijzigt, opnieuw inloggen is noodzakelij
 if ($_SESSION['loggedin'] == 1)
 {
 echo "<meta http-equiv='refresh' content='3600;URL=http://{$_SERVER['SERVER_NAME']}/sessionexpired'>";	
+}
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 3600)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
 }
 }
 /* CopyRight PowerChaos 2016 */
--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-session_start();
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 3600)) {
-    // last request was more than 30 minutes ago
-    session_unset();     // unset $_SESSION variable for the run-time 
-    session_destroy();   // destroy session data in storage
-}
 //Our functions to show the pages and other things, so do not remove
+require_once (getenv("DOCUMENT_ROOT")."/functions/sessions.php");
+sesions();
 include (getenv("DOCUMENT_ROOT")."/functions/include.php");
 //first we include our sesions , else users can not login , so do not remove :D
-sesions();
+
 //here we show the header , feel free to adjust to fit your own template (pages/template/)
 template("header");
 //my sidebar , depending on template you need it or not ( pages/template/)
