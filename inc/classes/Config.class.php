a26dc54f9db917530c7312d4abb02f9ecf039cfb
PowerChaos <PowerChaos@users.noreply.github.com>
2021-09-18 02:06:10 +0200

    Add files via upload

---
+++ inc/classes/Config.class.php
@@ -0,0 +1,8 @@
+<?php
+class Config {
+    const DB_HOST     	= 'localhost', // Database Host
+          DB_DATA	  	= 'database', // Database Name
+		  DB_USER     	= 'user', // Database User
+          DB_PASS     	= 'password', // Database Password
+}
+?>
