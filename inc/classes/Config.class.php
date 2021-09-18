bb8cdf8d916623581241077d36072908baa62d18
PowerChaos <PowerChaos@users.noreply.github.com>
2021-09-18 04:39:15 +0200

    Merge branch '2.0.0' of https://github.com/PowerChaos/vaporshop into 2.0.0

--- inc/classes/Config.class.php
+++ inc/classes/Config.class.php
@@ -1,8 +1,45 @@
 <?php
+
+/*
+<!#CR>
+************************************************************************************************************************
+*                                                    Copyrigths ©                                                      *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          Authors Names    > PowerChaos                                                                               *
+*          Company Name     > VPS Data                                                                                 *
+*          Company Email    > info@vpsdata.be                                                                          *
+*          Company Websites > https://vpsdata.be                                                                       *
+*                             https://vpsdata.shop                                                                     *
+*          Company Socials  > https://facebook.com/vpsdata                                                             *
+*                             https://twitter.com/powerchaos                                                           *
+*                             https://instagram.com/vpsdata                                                            *
+* -------------------------------------------------------------------------------------------------------------------- *
+*                                           File and License Informations                                              *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          File Name        > <!#FN> Config.class.php </#FN>                                                           
+*          File Birth       > <!#FB> 2021/09/02 09:22:30.072 </#FB>                                                    *
+*          File Mod         > <!#FT> 2021/09/18 03:33:50.785 </#FT>                                                    *
+*          License          > <!#LT> CC-BY-NC-ND-4.0 </#LT>                                                            
+*                             <!#LU> https://spdx.org/licenses/CC-BY-NC-ND-4.0.html </#LU>                             
+*                             <!#LD> This file may not be redistributed in whole or significant part. </#LD>           
+*          File Version     > <!#FV> 0.0.1 </#FV>                                                                      
+*                                                                                                                      *
+</#CR>
+*/
+
+
+
+
+
+
+
+
+
+
 class Config {
     const DB_HOST     	= 'localhost', // Database Host
-          DB_DATA	  	= 'database', // Database Name
-		  DB_USER     	= 'user', // Database User
-          DB_PASS     	= 'password', // Database Password
+          DB_DATA	  	= 'vpsd_webshop', // Database Name
+		  DB_USER     	= 'vpsd_WebShop', // Database User
+          DB_PASS     	= 'e*HJLtmzHGq%k#ya'; // Database Password
 }
 ?>
