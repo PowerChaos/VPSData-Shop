2c3c5d4e00f806f51a8630147b195b49df4c2f47
PowerChaos <PowerChaos@users.noreply.github.com>
2021-09-18 02:04:41 +0200

    added git ignore

--- inc/classes/Config.class.php
+++ inc/classes/Config.class.php
@@ -1,9 +1,9 @@
 <?php
 class Config {
     const DB_HOST     	= 'localhost', // Database Host
-          DB_DATA	  	= 'dir_links', // Database Name
-		  DB_USER     	= 'dir_link', // Database User
-          DB_PASS     	= 'DirectoryLinking', // Database Password
+          DB_DATA	  	= 'vpsd_webshop', // Database Name
+		  DB_USER     	= 'vpsd_WebShop', // Database User
+          DB_PASS     	= 'e*HJLtmzHGq%k#ya', // Database Password
 		  TMP		  	= 'tmp', // Public temp directory ( public folder )
 		  DOWNLOAD    	= '/home/file.cafe/public_html/uploads', // Download/upload folder ( full path, no end slash)
 		  DOWNLOADURL   = '/uploads', // download folder (no end slash , only folder)
