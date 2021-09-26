cc74367de50d770a8e583f07e344cfd9130526ba
PowerChaos <PowerChaos@users.noreply.github.com>
2021-09-26 02:54:08 +0200

    fixed voting

--- ajax/voting.php
+++ ajax/voting.php
@@ -1,4 +1,34 @@
 <?php
+
+/*
+<!#CR>
+************************************************************************************************************************
+*                                                    Copyrigths ©                                                      *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          Authors Names    > PowerChaos                                                                               *
+*          Company Name     > VPS Data                                                                                 *
+*          Company Email    > info@vpsdata.be                                                                          *
+*          Company Websites > https://vpsdata.be                                                                       *
+*                             https://vpsdata.shop                                                                     *
+*          Company Socials  > https://facebook.com/vpsdata                                                             *
+*                             https://twitter.com/powerchaos                                                           *
+*                             https://instagram.com/vpsdata                                                            *
+* -------------------------------------------------------------------------------------------------------------------- *
+*                                           File and License Informations                                              *
+* -------------------------------------------------------------------------------------------------------------------- *
+*          File Name        > <!#FN> voting.php </#FN>                                                                 
+*          File Birth       > <!#FB> 2021/09/18 00:38:17.351 </#FB>                                                    *
+*          File Mod         > <!#FT> 2021/09/26 02:53:49.356 </#FT>                                                    *
+*          License          > <!#LT> CC-BY-NC-ND-4.0 </#LT>                                                            
+*                             <!#LU> https://spdx.org/licenses/CC-BY-NC-ND-4.0.html </#LU>                             
+*                             <!#LD> This file may not be redistributed in whole or significant part. </#LD>           
+*          File Version     > <!#FV> 2.0.0 </#FV>                                                                      
+*                                                                                                                      *
+</#CR>
+*/
+
+
+
 $db = new Db;
 $perm = new Gebruikers;
 $session = new Session;
